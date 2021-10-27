d2c67077a28b456f2750d1a5e0a97346e5d6942b
ryan-sliger <ryan.sliger22@sitechhs.com>
2021-10-27 18:14:16 -0400

    at home

--- vueproject/src/App.vue
+++ vueproject/src/App.vue
@@ -1,24 +1,34 @@
 <template>
-  <div id="app">
+  <div id= "app">
     <h1 class="main">
       Welcome To T-Shirt Customizer
     </h1>
-
+    <div>
+      <img v-bind:src="image">
+    </div>
+  
   </div>
+    
+  
 </template>
 
 <script>
 
 export default {
   name: 'App',
-data(){
-  return{
-    logoname
-  }
-}
-    
-  
-}
+
+  data() {
+    return {
+      logo: [{
+        name: "nike",
+        image: require(nike.png)
+      },{
+        name: "under armor",
+        image: require(under_armor.png)
+      }]
+    };
+},
+};
 </script>
 
 <style>
