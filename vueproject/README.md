9d65fceeb09700952116872c098d5becf552d2fb
ryan-sliger <ryan.sliger22@sitechhs.com>
2021-10-27 11:44:40 -0400

    in class

--- vueproject/README.md
+++ vueproject/README.md
@@ -1,4 +1,4 @@
-# reactivity
+# vueproject
 
 ## Project setup
 ```
---
+++ vueproject/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h1 class="main">
+      Welcome To T-Shirt Customizer
+    </h1>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+data(){
+  return{
+    logoname
+  }
+}
+    
+  
+}
+</script>
+
+<style>
+
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #502c2c;
+  margin-top: 60px;
+}
+</style>
